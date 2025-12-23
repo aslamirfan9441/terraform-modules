d963717f3421c61fe2597e2229b29d8bf79aea97
aslamirfan9441 <aslamirfan9441@gmail.com>
2025-12-23 15:40:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
+# Single IAM user
+resource "aws_iam_user" "cloud" {
+  name = "cloud-user"
+}
 
+# Multiple IAM users using count
+resource "aws_iam_user" "cloud_count" {
+  count = 2
+  name  = "cloud-user-${count.index}"
+}
+
+# Multiple IAM users using for_each
+resource "aws_iam_user" "cloud_forset_user" {
+  for_each = toset(["alice", "bob", "charlie"])
+  name     = each.key
+}
