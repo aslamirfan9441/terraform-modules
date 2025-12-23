02f427ee4fb56b97d6890bede72d6a55164a0d7e
aslamirfan9441 <aslamirfan9441@gmail.com>
2025-12-23 15:31:47 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,7 @@
 output "iam_users" {
-  description = "IAM users created"
-  value = [
-    aws_iam_user.cloud.name,
-    aws_iam_user.cloud-count[*].name,
-    values(aws_iam_user.cloud-forset-user)[*].name
-  ]
+  value = {
+    single_user    = aws_iam_user.cloud.name
+    counted_users  = aws_iam_user.cloud_count[*].name
+    foreach_users  = [for u in aws_iam_user.cloud_forset_user : u.name]
+  }
 }
